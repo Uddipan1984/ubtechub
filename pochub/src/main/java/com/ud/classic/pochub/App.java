77d049f46a345e957c8f82222bd44bd71059002b
uddipanbasu <uddipanbasu@10.0.0.137>
2019-09-13 00:31:48 -0400

    First Git ====commit

---
+++ pochub/src/main/java/com/ud/classic/pochub/App.java
@@ -0,0 +1,13 @@
+package com.ud.classic.pochub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
