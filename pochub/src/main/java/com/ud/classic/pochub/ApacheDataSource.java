e929c5a2f114c2b768579c64965e472fb6366af7
uddipanbasu <uddipanbasu@10.0.0.137>
2019-10-14 21:32:06 -0400

    Commit for Apache data source pool

--- pochub/src/main/java/com/ud/classic/pochub/ApacheDataSource.java
+++ pochub/src/main/java/com/ud/classic/pochub/ApacheDataSource.java
@@ -1,5 +1,30 @@
 package com.ud.classic.pochub;
 
 public class ApacheDataSource {
+	
+	
+	public static void connect () {
+		
+	}
+	
+	
+	public static void init () {
+		
+	}
+	
+	
+	public void releaseConnections () throws Exception {
+		
+	}
+	
+	public Object getConnection () {
+		
+		return null;
+	}
+	
+    public Object getWraperConnection () {
+		
+		return null;
+	}
 
 }
